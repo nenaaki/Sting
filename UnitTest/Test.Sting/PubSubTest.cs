6d840329f9e86482f76b10dc713523708865d276
nenaaki <nenaaki@gmail.com>
2018-08-18 15:58:59 +0900

    PubSub ユニットテスト追加

---
+++ UnitTest/Test.Sting/PubSubTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Practices.Prism.PubSubEvents;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sting
+{
+    [TestClass]
+    public class PubSubTest
+    {
+        [TestMethod]
+        public void PubSubBasicTest()
+        {
+            bool result = false;
+
+            using (var sub = new Subscriber<bool>(args => result = args, ThreadOption.PublisherThread))
+            {
+                Publisher.Publish(true);
+
+                result.IsTrue();
+            }
+
+            Publisher.Publish(false);
+
+            result.IsTrue();
+        }
+    }
+}
